d4e9b067878cb817a86a5d3225df69ce02e427c5
PJ Fanning <pjfanning@users.noreply.github.com>
2023-10-28 21:07:42 +0100

    disable class registration for scala 3 test

--- src/test/scala-3/io/github/pjfanning/fury/FuryEnum3Test.scala
+++ src/test/scala-3/io/github/pjfanning/fury/FuryEnum3Test.scala
@@ -8,8 +8,8 @@ import org.scalatest.wordspec.AnyWordSpec
 class FuryEnum3Test extends AnyWordSpec with Matchers {
   "fury scala enum support" should {
     "serialize/deserialize ColorEnum" in {
-      val fury = Fury.builder().withLanguage(Language.JAVA).build()
-      fury.register(classOf[ColorEnum])
+      val fury = Fury.builder().withLanguage(Language.JAVA).requireClassRegistration(false).build()
+      //fury.register(classOf[ColorEnum])
       //fury.register(Class.forName("io.github.pjfanning.fury.ColorEnum$$anon$1"))
       val bytes = fury.serialize(ColorEnum.Green)
       fury.deserialize(bytes) shouldBe ColorEnum.Green
