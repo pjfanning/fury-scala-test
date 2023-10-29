22bcb3e8f77231cf27e1280e8f7792287dec080b
PJ Fanning <pjfanning@users.noreply.github.com>
2023-10-29 09:19:45 +0000

    Update FuryEnumeration212Test.scala

--- src/test/scala-2.12/io/github/pjfanning/fury/FuryEnumeration212Test.scala
+++ src/test/scala-2.12/io/github/pjfanning/fury/FuryEnumeration212Test.scala
@@ -5,12 +5,16 @@ import io.fury.config.Language
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.collection.mutable
+
 class FuryEnumeration212Test extends AnyWordSpec with Matchers {
   "fury scala enumeration support" should {
     "serialize/deserialize Weekday" in {
+      // withRefTracking(true) is needed or a StackOverflowError will happen
       // https://github.com/alipay/fury/issues/1032
-      val fury = Fury.builder().withLanguage(Language.JAVA).build()
+      val fury = Fury.builder().withLanguage(Language.JAVA).withRefTracking(true).build()
       fury.register(ScalaClasses.ScalaEnumerationValClass)
+      fury.register(classOf[mutable.HashMap[_, _]])
       fury.register(WeekDay.getClass)
       val bytes = fury.serialize(WeekDay.Wed)
       fury.deserialize(bytes) shouldBe WeekDay.Wed
